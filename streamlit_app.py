d218be002c5a3b0804cee08a6881734005263613
Krishna B Gowda <star08685@gmail.com>
2024-11-03 00:00:02 +0530

    Update streamlit_app.py
    Working on example of another Streamlit App (not our project) and the workflow between GitHub and Streamlit. trying for second time.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
 #st.title("My Parents New Healthy Diner")
