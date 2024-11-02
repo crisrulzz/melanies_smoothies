5598f98b0ddfd75edb4983506e5ad370cb15b0d3
Krishna B Gowda <star08685@gmail.com>
2024-11-03 00:02:35 +0530

    Update streamlit_app.py
    Working on example of another Streamlit App (not our project) and the workflow between GitHub and Streamlit. trying for fifth time.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('My Parents New Healthy Diner')
 
 #creating breakfast menu
 st.header('Breakfast Menu')
-st.write('- Omega 3 & Blueberry Oatmeal')
-st.write('- Kale, Spinach & Rocket Smoothie')
-st.write('- Hard-Boiled Free-Range Egg')
+st.write('Omega 3 & Blueberry Oatmeal')
+st.write('Kale, Spinach & Rocket Smoothie')
+st.write('Hard-Boiled Free-Range Egg')
